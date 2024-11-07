6cf862cf88300440fe3ff21834b77767a49a0bdc
glckfndr <obulan65@gmail.com>
2024-11-07 11:53:27 +0200

    feat: verify email uniqueness  in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,16 @@ const signup = async (req, res, next) => {
     //res.code = 201; // set response status code
     //throw new Error("Test error");
     const { name, email, password, role } = req.body;
+
+    const isUserExist = await User({ email });
+    if (isUserExist) {
+      return res.status(400).json({
+        code: 400,
+        status: false,
+        message: `User ${email} already exists`,
+      });
+    }
+
     const newUser = new User({ name, email, password, role });
     await newUser.save();
     res.status(201).json({
